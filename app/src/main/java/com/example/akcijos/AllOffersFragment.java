c3b5779fa0a5cc653ae24612ac197f5a60ada819
Tomvyc <tomas.vycas111@gmail.com>
2020-06-18 17:25:57 +0300

    Redo recycler view items

--- app/src/main/java/com/example/akcijos/AllOffersFragment.java
+++ app/src/main/java/com/example/akcijos/AllOffersFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -30,10 +31,15 @@ public class AllOffersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_all_offers, container, false);
+        return inflater.inflate(R.layout.fragment_all_offers, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
         // Set up the RecyclerView.
-        RecyclerView recyclerView = v.findViewById(R.id.recyclerview);
+        RecyclerView recyclerView = getView().findViewById(R.id.recyclerview);
         final OfferListAdapter offerListAdapter = new OfferListAdapter(getContext());
         recyclerView.setAdapter(offerListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -46,6 +52,5 @@ public class AllOffersFragment extends Fragment {
                 offerListAdapter.setOffers(offers);
             }
         });
-        return v;
     }
 }
