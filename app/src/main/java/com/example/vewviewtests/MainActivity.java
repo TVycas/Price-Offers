3e98bb35bdf39e66b508d4a8a3646786f60d28bc
Tomvyc <tomas.vycas111@gmail.com>
2020-06-18 15:46:53 +0300

    Add Toolbar with two fragments

--- app/src/main/java/com/example/vewviewtests/MainActivity.java
+++ app/src/main/java/com/example/vewviewtests/MainActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.List;
 
@@ -21,18 +23,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up the RecyclerView.
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        final OfferListAdapter adapter = new OfferListAdapter(this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // Inflate the Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create an instance of the tab layout from the view.
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        // Set the text for each tab.
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        // Set the tabs to fill the entire layout.
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Use PagerAdapter to manage page views in fragments.
+        // Each page is represented by its own fragment.
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter pagerAdapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+//        // Set up the RecyclerView.
+//        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+//        final OfferListAdapter offerListAdapter = new OfferListAdapter(this);
+//        recyclerView.setAdapter(offerListAdapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Observe ViewModel
         viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(MainActivityViewModel.class);
         viewModel.getAllOffers().observe(this, new Observer<List<Offer>>() {
             @Override
             public void onChanged(List<Offer> offers) {
-                adapter.setOffers(offers);
+//                offerListAdapter.setOffers(offers);
+            }
+        });
+
+        // Setting a listener for tab changes.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
             }
         });
     }
---
+++ app/src/main/java/com/example/vewviewtests/PagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.vewviewtests;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    int numOfTabs;
+
+    public PagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        this.numOfTabs = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new AllOffersFragment();
+            case 1:
+                return new UserChoicesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
