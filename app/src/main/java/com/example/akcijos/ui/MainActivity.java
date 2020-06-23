df98d3b404ddff87b835517fa26a2e6e38ecca19
Tomvyc <tomas.vycas111@gmail.com>
2020-06-23 15:49:28 +0300

    Add a TextView to notify when last refresh happened

--- app/src/main/java/com/example/akcijos/ui/MainActivity.java
+++ app/src/main/java/com/example/akcijos/ui/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.akcijos.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.akcijos.MainActivityViewModel;
 import com.example.akcijos.R;
+import com.example.akcijos.database.Offer;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
     private MainActivityViewModel viewModel;
+    private SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,13 +68,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        updateLastRefreshTextView();
+
         viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(MainActivityViewModel.class);
-        refreshDatabase();
+        viewModel.getAllOffers().observe(this, new Observer<List<Offer>>() {
+            @Override
+            public void onChanged(List<Offer> offers) {
+                updateLastRefreshTextView();
+            }
+        });
+
+    }
+
+    // Update the last refreshed text view to show when was the last offer refresh
+    private void updateLastRefreshTextView() {
+        TextView lastRefreshedTextView = findViewById(R.id.last_refreshed);
+
+        // Get stored time of last refresh
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String defaultRefreshDate = getString(R.string.default_refresh_date);
+        Long lastRefreshedDate = sharedPref.getLong(getString(R.string.saved_last_refresh_date_key), 0);
+
+        // Update the TextView based on the returned value
+        if (lastRefreshedDate != 0) {
+            Long currentTime = Calendar.getInstance().getTimeInMillis();
+            long timeDifference = currentTime - lastRefreshedDate;
+            int days = (int) (timeDifference / (1000 * 60 * 60 * 24));
+
+            lastRefreshedTextView.setText(getString(R.string.last_time_refreshed, days));
+        } else {
+            lastRefreshedTextView.setText(defaultRefreshDate);
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         if (item.getItemId() == R.id.refresh_offers) {// User chose the "Refresh Offers" item, running the scraping
+
+            // Save the offers refresh date
+            long date = Calendar.getInstance().getTimeInMillis();
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putLong(getString(R.string.saved_last_refresh_date_key), date);
+            editor.apply();
+
             refreshDatabase();
             return true;
         }
