355b1ce80ffa9bf48fe875570e0982fd0458a3f1
Tomvyc <tomas.vycas111@gmail.com>
2020-07-03 19:10:05 +0300

    Refactor Maxima Scraper

--- app/src/main/java/com/example/akcijos/scrapers/JavaScriptInterface.java
+++ app/src/main/java/com/example/akcijos/scrapers/JavaScriptInterface.java
@@ -1,20 +1,30 @@
 package com.example.akcijos.scrapers;
 
+import android.util.Log;
 import android.webkit.JavascriptInterface;
 
 import com.example.akcijos.database.Offer;
 
 import java.util.ArrayList;
 
-// An instance of this class will be registered as a JavaScript interface
+/**
+ * An instance of this class will be registered as a JavaScript interface.
+ */
 public class JavaScriptInterface {
 
     private ArrayList<Offer> maximaOffers = new ArrayList<>();
 
+    /**
+     * The method to start Maxima scraping. This method will be called from a separate WebView thread.
+     *
+     * @param html The fully loaded maxima html
+     */
     @JavascriptInterface
     public void scrapeMaximaHTML(String html) {
-        MaximaScraper sc = new MaximaScraper(html);
-        maximaOffers = sc.scrapeOffers();
+        Scraper scraper = new MaximaScraper(html);
+        Log.d("JavaScriptInterface", "scrapeMaximaHTML: " + scraper.getShopName() + " scraping started");
+        maximaOffers = scraper.scrapeOffers();
+        Log.d("JavaScriptInterface", "scrapeMaximaHTML: " + scraper.getShopName() + " scraping finished, loaded " + maximaOffers.size() + " offers.");
     }
 
     ArrayList<Offer> getMaximaOffers() {
