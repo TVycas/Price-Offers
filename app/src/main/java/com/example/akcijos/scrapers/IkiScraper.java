7cc596459daeecab52eb58a0e1dbef6d368e086f
Tomvyc <tomas.vycas111@gmail.com>
2020-07-03 19:09:59 +0300

    Refactor Maxima Scraper

--- app/src/main/java/com/example/akcijos/scrapers/IkiScraper.java
+++ app/src/main/java/com/example/akcijos/scrapers/IkiScraper.java
@@ -1,26 +1,35 @@
 package com.example.akcijos.scrapers;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class IkiScraper implements Scraper {
+import java.io.IOException;
+
+public class IkiScraper extends Scraper {
 
     private final String OFFERS_CONTAINER_CLASSNAME = "akcija__anchor akcija-inner";
     private final String SHOP_NAME = "Iki";
-    private String ikiUrl;
+    private final String IKI_URL;
 
     IkiScraper(String url) {
-        ikiUrl = url;
-    }
-
-    @Override
-    public String getOffersUrl() {
-        return ikiUrl;
+        IKI_URL = url;
     }
 
     @Override
     public String getShopName() {
         return SHOP_NAME;
+    }
+
+    @Override
+    Document getDocument() {
+        try {
+            return Jsoup.connect(IKI_URL).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -63,33 +72,35 @@ public class IkiScraper implements Scraper {
         Elements top = e.getElementsByClass("top");
         Elements priceOld = e.getElementsByClass("price-old");
 
-        // No price info, just the percentage
-        if (priceCents.size() == 0) {
-            Elements percentageElems = e.getElementsByClass("price-main");
-            String percentageString = percentageElems.get(0).text();
-            try {
+        try {
+            // No price info, just the percentage
+            if (priceCents.size() == 0) {
+                Elements percentageElems = e.getElementsByClass("price-main");
+                String percentageString = percentageElems.get(0).text();
+
                 if (percentageString.contains("iki")) {
                     percentage = Integer.parseInt(percentageString.substring(5, percentageString.length() - 1));
                 } else {
                     percentage = Integer.parseInt(percentageString.substring(1, percentageString.length() - 1));
                 }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            // Percentage is written at the top bar, above the price
-        } else if (top.size() != 0) {
-            String topString = top.get(0).getElementsByClass("text").text();
-            if (!topString.equals("Tik") && !topString.contains("+")) {
-                percentage = Integer.parseInt(topString.substring(1, topString.length() - 1));
-            }
-            // Not percentage information, but there is the old price so we can try calculating the percentage ourselves
-        } else if (priceOld.size() != 0) {
-            String oldPriceString = priceOld.text();
-            double oldPrice = Double.parseDouble(oldPriceString.substring(0, oldPriceString.length() - 2)
-                    + "." +
-                    oldPriceString.substring(oldPriceString.length() - 2));
+                // Percentage is written at the top bar, above the price
+            } else if (top.size() != 0) {
+                String topString = top.get(0).getElementsByClass("text").text();
+                if (!topString.equals("Tik") && !topString.contains("+")) {
+                    percentage = Integer.parseInt(topString.substring(1, topString.length() - 1));
+                }
+                // No percentage information, but there is the old price so we can try calculating the percentage ourselves
+            } else if (priceOld.size() != 0) {
+                String oldPriceString = priceOld.text();
+                double oldPrice = Double.parseDouble(oldPriceString.substring(0, oldPriceString.length() - 2)
+                        + "." +
+                        oldPriceString.substring(oldPriceString.length() - 2));
 
-            percentage = (int) Math.round(100 - (price * 100 / oldPrice));
+                percentage = (int) Math.round(100 - (price * 100 / oldPrice));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return -1;
         }
 
         return percentage;
